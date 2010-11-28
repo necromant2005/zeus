e9a31a83089758e097e391d041f08fe191c8c915
necromant <necromant@necromant.(none)>
2010-11-28 09:54:32 +0200

    making interface

---
+++ library/Zend/Db/Document/AbstractAdapter.php
@@ -0,0 +1,34 @@
+<?php
+namespace Zend\Db\Document;
+
+abstract class AbstractAdapter
+{
+    protected $_options = array();
+
+    protected $_connection = null;
+
+    public function __construct(array $options)
+    {
+        $this->_options = $options;
+    }
+
+    public function setConnection($connection)
+    {
+        $this->_connection = $connection;
+    }
+
+    public function getConnection()
+    {
+        return $this->_connection;
+    }
+
+    public function isConnected()
+    {
+        return !is_null($this->_connection);
+    }
+
+    abstract function _connect();
+
+    abstract function getCollection($name);
+}
+
---
+++ tests/Zend/Db/Document/_files/CollectionMock.php
@@ -0,0 +1,12 @@
+<?php
+namespace Test;
+
+use Zend\Db\Document as DbDocument;
+
+require_once __DIR__ . '/../../../../../library/Zend/Db/Document/Collection.php';
+
+class CollectionMock implements DbDocument\Collection
+{
+
+}
+
