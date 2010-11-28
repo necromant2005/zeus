61c66ac3c5c15127b71a2dc59f3c1f9c245de912
Necromant2005 <necromant2005@gmail.com>
2010-11-28 21:06:02 +0200

    add riak

---
+++ library/Zend/Db/Document/Adapter/Riak.php
@@ -0,0 +1,21 @@
+<?php
+namespace Zend\Db\Document\Adapter;
+
+use Zend\Db\Document as DbDocument;
+
+class Riak extends DbDocument\AbstractAdapter
+{
+    protected function _connect()
+    {
+        if ($this->isConnected()) return;
+        $this->_connection = new \Zend\Rest\Client\RestClient('http://'
+            . $this->_options['host'] . ':' . $this->_options['port']
+            . '/riak');
+    }
+
+    public function getCollection($name)
+    {
+
+    }
+}
+
