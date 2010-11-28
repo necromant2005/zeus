be2795336fe84e847c0b94108558dff5a7760a2c
Necromant2005 <necromant2005@gmail.com>
2010-11-28 18:01:09 +0200

    init mongo adapter

--- tests/Zend/Db/Document/Adapter/MongoTest.php
+++ tests/Zend/Db/Document/Adapter/MongoTest.php
@@ -9,9 +9,21 @@ require_once __DIR__ . '/../../../../../library/Zend/Db/Document/Adapter/Mongo.p
 
 class MongoTest extends \PHPUnit_Framework_TestCase
 {
-    public function testConnection()
+    public function setUp()
+    {
+        if (!defined('TESTS_ZEND_DB_DOCUMENT_ADAPTER_MONGO_ENABLED') || !TESTS_ZEND_DB_DOCUMENT_ADAPTER_MONGO_ENABLED) {
+            $this->markTestSkipped('The MongoDb extension is not available.');
+        }
+    }
+
+    public function testInit()
     {
         $adapter = new DbDocument\Adapter\Mongo();
+    }
+
+    public function testGetConnection()
+    {
+        $adapter = new DbDocument\Adapter\Mongo(array());
     }
 }
 
